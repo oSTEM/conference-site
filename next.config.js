489aedd0c6c4ffbd7bae88cbd86711900619bba9
Travis DePrato <travisdeprato@gmail.com>
2021-08-18 16:51:47 -0700

    Add remark-gfm plugin (support tables!)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
+// NOTE:
+//    We currently use remark-gfm@^1.0.0 since version 2.0.0 requires being
+//    loaded as an ESM module (which we can't do in next.config.js).
+//    This plugin provides support for tables, to-do lists, and auto-linking.
+const remarkGfm = require("remark-gfm");
+
 const withMDX = require("@next/mdx")({
   options: {
-    remarkPlugins: [],
+    remarkPlugins: [remarkGfm],
     rehypePlugins: [],
   },
 });
+
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "tsx", "mdx"],
   target: "serverless",
