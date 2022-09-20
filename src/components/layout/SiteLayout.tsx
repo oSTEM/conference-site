9b0b95a4777f4c446075305ee66f9169232c7103
Janine vN <janine9vn@gmail.com>
2022-09-19 21:46:05 -0400

    Add schedule page to sidebar

--- src/components/layout/SiteLayout.tsx
+++ src/components/layout/SiteLayout.tsx
@@ -111,6 +111,9 @@ const NavBarLinks = () => {
       <NavLink href={"/"} color={Color.AntiqueTeal}>
         Conference Home
       </NavLink>
+      <NavLink href={"/2022/schedule"} color={Color.AntiqueTeal}>
+        Schedule
+      </NavLink>
       <NavLink href={"/2022/registration"} color={Color.AntiqueTeal}>
         Registration
       </NavLink>
