5914556ec5348e5533c7107867d8e6145046debc
Janine vN <janine9vn@gmail.com>
2022-11-11 15:22:32 -0500

    Schedule updste and re-lock

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,24 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare module "*.svg" {
-  import * as React from "react";
-
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement>
-  >;
-
-  export default ReactComponent;
-}
-
-// Fix for https://github.com/vercel/styled-jsx/issues/698
-declare module "styled-jsx/css" {
-  declare const css: {
-    global(chunks: TemplateStringsArray, ...args: any[]): JSX.Element;
-    resolve(
-      chunks: TemplateStringsArray,
-      ...args: any[]
-    ): { className: string; styles: JSX.Element };
-  };
-  export default css;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
