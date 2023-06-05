e88c8580f5f156ba9a6e205453c4bf0274ec714f
hamburgersct <hamburgersct@gmail.com>
2023-06-05 10:48:58 -0400

    Test on Preview

--- src/components/layout/PhotoGallery.tsx
+++ src/components/layout/PhotoGallery.tsx
@@ -29,38 +29,53 @@ export const PhotoGallery = () => {
         width={600}
         height={400}
       />
-      <img
+      <Image
         src="/images/recap/2022/2022 Strategic Aliance - Ansys.jpg"
         width={600}
         height={400}
       />
-      <img
+      <Image
         src="/images/recap/2022/2022-11-12 oSTEM_0057.jpg"
         width={600}
         height={400}
       />
-      <img
+      <Image
         src="/images/recap/2022/2022-11-12 oSTEM_0107.jpg"
         width={600}
         height={400}
       />
-      <img
+      <Image
         src="/images/recap/2022/2022-11-12 oSTEM_0440 (1).jpg"
         width={600}
         height={400}
       />
-      <img
+      <Image
         src="/images/recap/2022/2022-11-12 oSTEM_0690.jpg"
         width={600}
         height={400}
       />
-      <img
+      <Image
         src="/images/recap/2022/2022-11-12 oSTEM_0755.jpg"
         width={600}
         height={400}
       />
-      <img
+      <Image
         src="/images/recap/2022/2022-11-12 oSTEM_1144.jpg"
+        width={600}
+        height={400}
+      />
+      <Image
+        src="/images/recap/2022/2022-11-12 oSTEM_1241.jpg"
+        width={600}
+        height={400}
+      />
+      <Image
+        src="/images/recap/2022/2022-11-12 oSTEM_1262.jpg"
+        width={600}
+        height={400}
+      />
+      <Image
+        src="/images/recap/2022/2022-11-12 oSTEM_1340.jpg"
         width={600}
         height={400}
       />
