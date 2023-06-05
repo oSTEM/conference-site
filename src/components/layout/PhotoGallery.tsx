337033849065fdd548055daa837ad136ded89cb1
hamburgersct <hamburgersct@gmail.com>
2023-06-05 10:27:05 -0400

    Deploy to Vercel (to test)

---
+++ src/components/layout/PhotoGallery.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Image from "next/image";
+
+export const PhotoGallery = () => {
+  return (
+    <div className={"grid gap-4 md:grid-cols-3 my-1"}>
+      <Image
+        src="/images/recap/2022/2022 Award - Angie Gonzalez.jpg"
+        width={600}
+        height={400}
+      />
+      <Image
+        src="/images/recap/2022/2022 Chapter Leader of the Year - Lex Yu at USC Quest.jpg"
+        width={600}
+        height={400}
+      />
+      <Image
+        src="/images/recap/2022/2022 Chapter of the Year.jpg"
+        width={600}
+        height={400}
+      />
+      <Image
+        src="/images/recap/2022/2022 Partner Excellence Award - Casady.jpg"
+        width={600}
+        height={400}
+      />
+      <Image
+        src="/images/recap/2022/2022 Rookie Chapter of the Year - Cal Poly San Luis.jpg"
+        width={600}
+        height={400}
+      />
+      <img
+        src="/images/recap/2022/2022 Strategic Aliance - Ansys.jpg"
+        width={600}
+        height={400}
+      />
+      <img
+        src="/images/recap/2022/2022-11-12 oSTEM_0057.jpg"
+        width={600}
+        height={400}
+      />
+      <img
+        src="/images/recap/2022/2022-11-12 oSTEM_0107.jpg"
+        width={600}
+        height={400}
+      />
+      <img
+        src="/images/recap/2022/2022-11-12 oSTEM_0440 (1).jpg"
+        width={600}
+        height={400}
+      />
+      <img
+        src="/images/recap/2022/2022-11-12 oSTEM_0690.jpg"
+        width={600}
+        height={400}
+      />
+      <img
+        src="/images/recap/2022/2022-11-12 oSTEM_0755.jpg"
+        width={600}
+        height={400}
+      />
+      <img
+        src="/images/recap/2022/2022-11-12 oSTEM_1144.jpg"
+        width={600}
+        height={400}
+      />
+    </div>
+  );
+};
