1615ee0fac11bd30cc124edae1b582b8ed7d6f4d
hamburgersct <hamburgersct@gmail.com>
2023-06-07 15:18:33 -0400

    Add hyperlink to each award category

--- src/components/awards/AwardSquare.tsx
+++ src/components/awards/AwardSquare.tsx
@@ -8,16 +8,20 @@ export const AwardSquare = ({
   borderColor = "border-red-500",
   children,
   title,
+  href,
 }: {
   children: React.ReactNode;
   title: string;
   borderColor: string;
+  href: string;
 }) => {
   return (
     <div
       className={`border-solid pl-4 border-l-8 ${borderColor} flex flex-col min-h-[10rem] py-2 my-4`}
     >
-      <h3>{title}</h3>
+      <a href={href}>
+        <h3>{title}</h3>
+      </a>
       {children}
     </div>
   );
