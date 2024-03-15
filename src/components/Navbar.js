6510f951717b5f757a1dce1bee2b996a91ad16a1
Rem Zhang <hello@itsrem.org>
2024-03-14 19:38:32 -0600

    Force-fix icon jankiness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,10 @@ export function DropdownExample() {
         <div>
           <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
             Say Hello
-            <FontAwesomeIcon icon={faChevronDown} fixedWidth />
+            <FontAwesomeIcon
+              className={styles["dropdownIcon"]}
+              icon={faChevronDown}
+            />
           </Menu.Button>
         </div>
         <Transition
