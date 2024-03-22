2e344804f5e9e11276fee63d50334ea30e71a2ac
Rem Zhang <hello@itsrem.org>
2024-03-22 11:17:37 -0600

    Navbar work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,9 +19,9 @@ function DropdownLinkExample() {
     { href: "/", label: "[dev] Home" },
   ];
   return (
-    <div className="top-16 mr-4 text-right">
+    <div className="mt-2.5 mr-4 text-right">
       <Menu as="div" className="relative inline-block text-left">
-        <Menu.Button className="inline-flex w-full justify-center rounded-md border-2 border-transparent px-4 py-2 text-sm font-medium hover:bg-green-600/20 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+        <Menu.Button className="inline-flex w-full justify-center rounded-full border-2 border-green-600 px-4 py-2 text-sm font-medium hover:bg-green-600/20 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/75 transition">
           Attend{" "}
           <FontAwesomeIcon
             className={styles["dropdownIcon"]}
@@ -49,7 +49,7 @@ function DropdownLinkExample() {
                         active
                           ? "bg-green-700 text-white"
                           : "bg-white text-black"
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm transition`}
                     >
                       {link.label}
                     </a>
@@ -64,99 +64,32 @@ function DropdownLinkExample() {
   );
 }
 
-export function DropdownExample() {
-  return (
-    <div className="top-16 w-56 text-right">
-      <Menu as="div" className="relative inline-block text-left">
-        <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-            Say Hello
-            <FontAwesomeIcon
-              className={styles["dropdownIcon"]}
-              icon={faChevronDown}
-            />
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-            <div className="px-1 py-1 ">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? "bg-purple text-white" : "text-gray-900"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    Hello world!
-                  </button>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href={"/2024/partnership"}>
-                    <button
-                      className={`${
-                        active ? "bg-purple text-white" : "text-gray-900"
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                    >
-                      Partnerships
-                    </button>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? "bg-purple text-white" : "text-gray-900"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    Hi Everyone Else
-                  </button>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? "bg-purple text-white" : "text-gray-900"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    Goodbye
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </div>
-  );
-}
-
 export default function NavBar() {
   return (
     <div className={styles.NavNew}>
-      <div className={styles["NavNew-inner"]}>
-        <div className={styles["NavNew-inner-left"]}>
+      <div className="flex pb-1 border-b border-black max-w-7xl mx-auto">
+        <div className={`flex ${styles["NavNew-inner-left"]}`}>
           <Link href="/">
             <a className="inline-block">
               <img
                 alt="oSTEM"
                 src="/logo-banner.png"
-                className="flex max-w-[12rem] mx-auto"
+                className="flex h-14 mt-0.5 mx-auto"
               />
             </a>
           </Link>
-          <p className="inline-block">14th Annual Conference</p>
+          <div className="ml-2 pl-2 border-l border-black mt-2">
+            <p className="inline-block text-xl leading-none pb-0">
+              <b>
+                14<sup>th</sup>
+              </b>{" "}
+              Annual Conference
+              <br></br>
+              <span className={`text-sm ${styles["gradientText"]}`}>
+                <b>Portland, OR</b> - Oct 17-20, 2024
+              </span>
+            </p>
+          </div>
         </div>
         <div className={styles["NavNew-inner-right"]}>
           {/* <DropdownLinkExample /> */}
