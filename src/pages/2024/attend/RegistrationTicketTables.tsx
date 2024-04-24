1168c998590cc368a8e072317510c4272a2b569a
Rem Zhang <hello@itsrem.org>
2024-04-24 11:33:36 -0600

    Image improvements

--- src/pages/2024/attend/RegistrationTicketTables.tsx
+++ src/pages/2024/attend/RegistrationTicketTables.tsx
@@ -8,50 +8,54 @@ export const CollegiateTable = ({ includeFees }: { includeFees?: boolean }) => {
   return (
     <div>
       <table className='table-fixed text-center table-mb'>
-        <tr className='bg-green-300/25'>
-          <th>Ticket Type</th>
-          <th>Purchase Deadline</th>
-          <th>Member Price</th>
-          <th>Non-Member Price</th>
-        </tr>
-        <tr>
-          <td>
-            Collegiate Full Conference{' '}
-            <TextBadge className='border-nav-green text-nav-green'>
-              Early Bird
-            </TextBadge>
-          </td>
-          <td>June 30, 2024</td>
-          <td>$150</td>
-          <td>$225</td>
-        </tr>
-        <tr className='bg-green-300/10'>
-          <td>
-            Collegiate Full Conference <TextBadge>Regular</TextBadge>
-          </td>
-          <td>September 30, 2024</td>
-          <td>$175</td>
-          <td>$250</td>
-        </tr>
-        <tr>
-          <td>
-            Advisor Full Conference{' '}
-            <TextBadge className='border-nav-green text-nav-green'>
-              Early Bird
-            </TextBadge>
-          </td>
-          <td>June 30, 2024</td>
-          <td>$225</td>
-          <td>n/a</td>
-        </tr>
-        <tr className='bg-green-300/10'>
-          <td>
-            Advisor Full Conference <TextBadge>Regular</TextBadge>
-          </td>
-          <td>September 30, 2024</td>
-          <td>$250</td>
-          <td>n/a</td>
-        </tr>
+        <thead>
+          <tr className='bg-green-300/25'>
+            <th>Ticket Type</th>
+            <th>Purchase Deadline</th>
+            <th>Member Price</th>
+            <th>Non-Member Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Collegiate Full Conference{' '}
+              <TextBadge className='border-nav-green text-nav-green'>
+                Early Bird
+              </TextBadge>
+            </td>
+            <td>June 30, 2024</td>
+            <td>$150</td>
+            <td>$225</td>
+          </tr>
+          <tr className='bg-green-300/10'>
+            <td>
+              Collegiate Full Conference <TextBadge>Regular</TextBadge>
+            </td>
+            <td>September 30, 2024</td>
+            <td>$175</td>
+            <td>$250</td>
+          </tr>
+          <tr>
+            <td>
+              Advisor Full Conference{' '}
+              <TextBadge className='border-nav-green text-nav-green'>
+                Early Bird
+              </TextBadge>
+            </td>
+            <td>June 30, 2024</td>
+            <td>$225</td>
+            <td>n/a</td>
+          </tr>
+          <tr className='bg-green-300/10'>
+            <td>
+              Advisor Full Conference <TextBadge>Regular</TextBadge>
+            </td>
+            <td>September 30, 2024</td>
+            <td>$250</td>
+            <td>n/a</td>
+          </tr>
+        </tbody>
       </table>
       <p className='mt-0'>
         *Prices displayed do not include registration fees.{' '}
@@ -61,7 +65,7 @@ export const CollegiateTable = ({ includeFees }: { includeFees?: boolean }) => {
   );
 };
 
-const GeneralTable = ({ table }: { table: String }) => {
+const GeneralTable = ({ type }: { type: String }) => {
   return <CollegiateTable></CollegiateTable>;
 };
 
