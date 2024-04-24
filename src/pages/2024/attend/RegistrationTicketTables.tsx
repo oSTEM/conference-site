7b98e5e76c9fbbc15fe550c2f481349ba50c41ce
Rem Zhang <hello@itsrem.org>
2024-04-24 13:36:16 -0600

    more work

--- src/pages/2024/attend/RegistrationTicketTables.tsx
+++ src/pages/2024/attend/RegistrationTicketTables.tsx
@@ -6,67 +6,74 @@ const Price = ({ val }: { val: number }) => {
 
 export const CollegiateTable = ({ includeFees }: { includeFees?: boolean }) => {
   return (
-    <div>
-      <table className='table-fixed text-center table-mb'>
-        <thead>
-          <tr className='bg-green-300/25'>
-            <th>Ticket Type</th>
-            <th>Purchase Deadline</th>
-            <th>Member Price</th>
-            <th>Non-Member Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              Collegiate Full Conference{' '}
-              <TextBadge className='border-nav-green text-nav-green'>
-                Early Bird
-              </TextBadge>
-            </td>
-            <td>June 30, 2024</td>
-            <td>$150</td>
-            <td>$225</td>
-          </tr>
-          <tr className='bg-green-300/10'>
-            <td>
-              Collegiate Full Conference <TextBadge>Regular</TextBadge>
-            </td>
-            <td>September 30, 2024</td>
-            <td>$175</td>
-            <td>$250</td>
-          </tr>
-          <tr>
-            <td>
-              Advisor Full Conference{' '}
-              <TextBadge className='border-nav-green text-nav-green'>
-                Early Bird
-              </TextBadge>
-            </td>
-            <td>June 30, 2024</td>
-            <td>$225</td>
-            <td>n/a</td>
-          </tr>
-          <tr className='bg-green-300/10'>
-            <td>
-              Advisor Full Conference <TextBadge>Regular</TextBadge>
-            </td>
-            <td>September 30, 2024</td>
-            <td>$250</td>
-            <td>n/a</td>
-          </tr>
-        </tbody>
-      </table>
-      <p className='mt-0'>
-        *Prices displayed do not include registration fees.{' '}
-        <span>Show Prices w/ Fees</span>
-      </p>
-    </div>
+    <table className='table-fixed text-center table-mb'>
+      <thead>
+        <tr className='bg-green-300/25'>
+          <th>Ticket Type</th>
+          <th>Purchase Deadline</th>
+          <th>Member Price</th>
+          <th>Non-Member Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            Collegiate Full Conference{' '}
+            <TextBadge className='border-nav-green text-nav-green'>
+              Early Bird
+            </TextBadge>
+          </td>
+          <td>June 30, 2024</td>
+          <td>$150</td>
+          <td>$225</td>
+        </tr>
+        <tr className='bg-green-300/10'>
+          <td>
+            Collegiate Full Conference{' '}
+            <TextBadge className='border-text-color'>Regular</TextBadge>
+          </td>
+          <td>September 30, 2024</td>
+          <td>$175</td>
+          <td>$250</td>
+        </tr>
+        <tr>
+          <td>
+            Advisor Full Conference{' '}
+            <TextBadge className='border-nav-green text-nav-green'>
+              Early Bird
+            </TextBadge>
+          </td>
+          <td>June 30, 2024</td>
+          <td>$225</td>
+          <td>n/a</td>
+        </tr>
+        <tr className='bg-green-300/10'>
+          <td>
+            Advisor Full Conference{' '}
+            <TextBadge className='border-text-color'>Regular</TextBadge>
+          </td>
+          <td>September 30, 2024</td>
+          <td>$250</td>
+          <td>n/a</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
 const GeneralTable = ({ type }: { type: String }) => {
-  return <CollegiateTable></CollegiateTable>;
+  return (
+    <div>
+      <p>
+        Tickets are{' '}
+        <span className='text-nav-green' title='6.08% + $1.32/ticket'>
+          subject to a fee
+        </span>{' '}
+        not shown below.
+      </p>
+      <CollegiateTable></CollegiateTable>
+    </div>
+  );
 };
 
 export default GeneralTable;
