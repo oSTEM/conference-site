b543a52fbd110b8149c1cbe70c3fc6546a7eeebc
Rem Zhang <hello@itsrem.org>
2024-04-24 22:47:08 -0600

    Finish ticket tables

--- src/pages/2024/attend/RegistrationTicketTables.tsx
+++ src/pages/2024/attend/RegistrationTicketTables.tsx
@@ -33,8 +33,38 @@ const ToggleSwitch = ({
   );
 };
 
+const PriceElement = ({
+  val,
+  fee,
+  showFee,
+}: {
+  val: number;
+  fee: number;
+  showFee: boolean;
+}) => {
+  return showFee ? (
+    <td
+      className='text-teal-800 dark:text-teal-100 cursor-help'
+      title={`Price shown includes a transaction fee of $${fee.toFixed(
+        2,
+      )} per ticket.`}
+    >
+      ${(val + fee).toFixed(2)}
+    </td>
+  ) : (
+    <td
+      className='cursor-help'
+      title={`Price shown does not include a $${fee.toFixed(
+        2,
+      )} transaction fee.`}
+    >
+      ${val} <span className='opacity-80 text-[12px]'> + Fees</span>
+    </td>
+  );
+};
+
 export const CollegiateTable = ({ includeFees }: { includeFees?: boolean }) => {
-  const [enabled, setEnabled] = useState(false);
+  const [showFee, setShowFee] = useState(false);
 
   return (
     <table className='text-center table-mb'>
@@ -45,16 +75,18 @@ export const CollegiateTable = ({ includeFees }: { includeFees?: boolean }) => {
               <span className='grow'>
                 Tickets are{' '}
                 <span
-                  className='text-nav-green'
+                  className='text-nav-green cursor-help'
                   title='Approx. 6.08% + $1.32/ticket'
                 >
                   subject to a fee
                 </span>{' '}
-                not currently shown below.
+                {showFee
+                  ? 'included in the prices shown.'
+                  : 'not currently shown below.'}
               </span>
               <span>
                 Show Prices w/ Fees{' '}
-                <ToggleSwitch enabled={enabled} setEnabled={setEnabled} />
+                <ToggleSwitch enabled={showFee} setEnabled={setShowFee} />
               </span>
             </span>
           </th>
@@ -75,8 +107,8 @@ export const CollegiateTable = ({ includeFees }: { includeFees?: boolean }) => {
             </TextBadge>
           </td>
           <td>June 30, 2024</td>
-          <td>$150</td>
-          <td>$225</td>
+          <PriceElement val={150} fee={10.44} showFee={showFee}></PriceElement>
+          <PriceElement val={225} fee={15} showFee={showFee}></PriceElement>
         </tr>
         <tr>
           <td>
@@ -84,8 +116,8 @@ export const CollegiateTable = ({ includeFees }: { includeFees?: boolean }) => {
             <TextBadge className='border-text-color'>Regular</TextBadge>
           </td>
           <td>September 30, 2024</td>
-          <td>$175</td>
-          <td>$250</td>
+          <PriceElement val={175} fee={11.96} showFee={showFee}></PriceElement>
+          <PriceElement val={250} fee={16.52} showFee={showFee}></PriceElement>
         </tr>
         <tr>
           <td>
@@ -95,7 +127,7 @@ export const CollegiateTable = ({ includeFees }: { includeFees?: boolean }) => {
             </TextBadge>
           </td>
           <td>June 30, 2024</td>
-          <td>$225</td>
+          <PriceElement val={225} fee={15.0} showFee={showFee}></PriceElement>
           <td>n/a</td>
         </tr>
         <tr>
@@ -104,7 +136,7 @@ export const CollegiateTable = ({ includeFees }: { includeFees?: boolean }) => {
             <TextBadge className='border-text-color'>Regular</TextBadge>
           </td>
           <td>September 30, 2024</td>
-          <td>$250</td>
+          <PriceElement val={250} fee={16.52} showFee={showFee}></PriceElement>
           <td>n/a</td>
         </tr>
       </tbody>
@@ -112,8 +144,80 @@ export const CollegiateTable = ({ includeFees }: { includeFees?: boolean }) => {
   );
 };
 
+export const ProfessionalTable = ({
+  includeFees,
+}: {
+  includeFees?: boolean;
+}) => {
+  const [showFee, setShowFee] = useState(false);
+
+  return (
+    <table className='text-center table-mb'>
+      <thead>
+        <tr>
+          <th className='p-0 pb-1 font-normal' colSpan={4}>
+            <span className='flex w-full text-left'>
+              <span className='grow'>
+                Tickets are{' '}
+                <span
+                  className='text-nav-green cursor-help'
+                  title='Approx. 6.08% + $1.32/ticket'
+                >
+                  subject to a fee
+                </span>{' '}
+                {showFee
+                  ? 'included in the prices shown.'
+                  : 'not currently shown below.'}
+              </span>
+              <span>
+                Show Prices w/ Fees{' '}
+                <ToggleSwitch enabled={showFee} setEnabled={setShowFee} />
+              </span>
+            </span>
+          </th>
+        </tr>
+        <tr className='bg-green-400/30'>
+          <th>Ticket Type</th>
+          <th>Purchase Deadline</th>
+          <th>Member Price</th>
+          <th>Non-Member Price</th>
+        </tr>
+      </thead>
+      <tbody className='[&>*:nth-child(2n)]:bg-green-400/15'>
+        <tr>
+          <td>
+            Professional Full Conference{' '}
+            <TextBadge className='border-nav-green text-nav-green'>
+              Early Bird
+            </TextBadge>
+          </td>
+          <td>June 30, 2024</td>
+          <PriceElement val={375} fee={24.11} showFee={showFee}></PriceElement>
+          <PriceElement val={425} fee={27.15} showFee={showFee}></PriceElement>
+        </tr>
+        <tr>
+          <td>
+            Professional Full Conference{' '}
+            <TextBadge className='border-text-color'>Regular</TextBadge>
+          </td>
+          <td>September 30, 2024</td>
+          <PriceElement val={475} fee={30.19} showFee={showFee}></PriceElement>
+          <PriceElement val={525} fee={33.23} showFee={showFee}></PriceElement>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
 const GeneralTable = ({ type }: { type: String }) => {
-  return <CollegiateTable></CollegiateTable>;
+  switch (type) {
+    case 'collegiate':
+      return <CollegiateTable></CollegiateTable>;
+    case 'professional':
+      return <ProfessionalTable></ProfessionalTable>;
+    default:
+      return <div>Table {type} not valid.</div>;
+  }
 };
 
 export default GeneralTable;
