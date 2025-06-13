b6f3df535673ec68464f5958b08c54f96b288904
juan <juan@cu-ecae-128.138.158.177.colorado.edu>
2025-06-13 16:23:49 -0600

    fixed posterss typo

--- src/components/navbar/Navbar.config.ts
+++ src/components/navbar/Navbar.config.ts
@@ -83,7 +83,7 @@ export const NAVBAR_LINKS: NavbarLink[] = [
   },
   {
     category: 'present',
-    label: 'Call for Posterss',
+    label: 'Call for Posters',
     href: '/2025/present/call-for-posters',
     badge: {
       label: 'Open',
